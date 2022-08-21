8c755fd55877ea25cce978cc01efcf2c6dab36d8
bubuding0809 <bubuding0809@gmail.com>
2022-08-21 19:48:26 +0800

    Setup prisma db

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,57 @@
-import Link from 'next/link'
-import Layout from '../components/Layout'
+import Layout from "../components/Layout";
+import { PrismaClient, User } from "@prisma/client";
+import { useEffect, useState } from "react";
 
-const IndexPage = () => (
-  <Layout title="Home | Next.js + TypeScript Example">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">
-        <a>About</a>
-      </Link>
-    </p>
-  </Layout>
-)
+const prisma = new PrismaClient();
 
-export default IndexPage
+export async function getServerSideProps() {
+  const users: User[] = await prisma.user.findMany();
+
+  return {
+    props: {
+      users: JSON.parse(JSON.stringify(users)),
+    },
+  };
+}
+
+const IndexPage = ({ users }) => {
+  const [userData, setUserData] = useState<User[]>(users);
+
+  useEffect(() => {
+    const interval = setInterval(async () => {
+      try {
+        const response = await fetch("/api/testapi");
+        setUserData(await response.json());
+      } catch (error) {
+        console.log(error);
+      }
+    }, 3000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Layout title="Home | Next.js + TypeScript Example">
+      <h1>Hello Next.js 👋</h1>
+      <div className="flex flex-col bg-stone-400 p-4">
+        {userData.map(user => {
+          return (
+            <div key={user.id} className="flex flex-col p-4 bg-white my-2">
+              <div className="flex">
+                <div className="flex-1">
+                  <h2 className="text-2xl font-bold">{user.name}</h2>
+                  <p className="text-sm">{user.email}</p>
+                  <p className="text-sm">
+                    Created at {new Date(user.createdAt).toDateString()}
+                  </p>
+                </div>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </Layout>
+  );
+};
+
+export default IndexPage;
