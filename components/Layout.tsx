61fb5154982fa676f6d45dfcc76727e5dd87a99a
bubuding0809 <bubuding0809@gmail.com>
2022-08-21 16:38:11 +0800

    initial commit

---
+++ components/Layout.tsx
@@ -0,0 +1,41 @@
+import React, { ReactNode } from "react";
+import Link from "next/link";
+import Head from "next/head";
+
+type Props = {
+  children?: ReactNode;
+  title?: string;
+};
+
+const Layout = ({ children, title = "This is the default title" }: Props) => (
+  <div className="h-screen flex flex-col">
+    <Head>
+      <title>{title}</title>
+      <meta charSet="utf-8" />
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <header className="bg-slate-50 shadow-md h-10 p-2">
+      <nav>
+        <Link href="/">
+          <a>Home</a>
+        </Link>{" "}
+        |{" "}
+        <Link href="/about">
+          <a>About</a>
+        </Link>{" "}
+        |{" "}
+        <Link href="/users">
+          <a>Users List</a>
+        </Link>{" "}
+        | <a href="/api/users">Users API</a>
+      </nav>
+    </header>
+    <div className="h-full p-2">{children}</div>
+    <footer className="flex justify-center items-center bg-slate-50 h-10 sticky bottom-0">
+      <hr />
+      <span>I'm here to stay (Footer)</span>
+    </footer>
+  </div>
+);
+
+export default Layout;
